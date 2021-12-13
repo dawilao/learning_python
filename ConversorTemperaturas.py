7e2cb8dd13cabfeb79851c0ea2ebb692b7f9a5eb
dawilao <96063474+dawilao@users.noreply.github.com>
2021-12-13 10:56:07 -0300

    Add files via upload
    Criando um conversor de temperaturas (°C <> °F) no Python.

---
+++ ConversorTemperaturas.py
@@ -0,0 +1,28 @@
+'''Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. 
+   C = 5 * ((F-32) / 9).
+   Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+'''
+print("CONVERSOR CELSIUS <> FAHRENHEIT")
+print("###############################")
+
+opcao = 1
+
+while opcao != 0:
+    print("\nDigite:\n1. Celsius para Fahrenheit\n2. Fahrenheit para Celsius")
+    print("Para sair, digite 0")
+    opcao = int(input("Opção: "))
+
+    if opcao == 1:
+        c = float(input("Digite a temperatura em °C: "))
+        f = (c * (9/5)) + 32
+        f = round(f,2)
+        print("\nA temperatura é de:", f,"°F")
+    elif opcao == 2:
+        f = float(input("Digite a temperatura em °F: "))
+        c = 5 * ((f-32) / 9)
+        c = round(c,2)
+        print("\nA temperatura é de:", c,"°C")
+    elif opcao == 0:
+        print("\nSaindo...")
+    else:
+        print("\nOpção incorreta. Tente novamente.")
