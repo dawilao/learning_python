5adf028a84e9ec7bc0c2058f959b92b450e75f73
Dawison Oliveira <96063474+dawilao@users.noreply.github.com>
2021-12-14 13:17:28 -0300

    Conversor de temperaturas
    Algoritmo que converte temperaturas em Celcius <> Fahrenheit.

--- ConversorTemperaturas.py
+++ ConversorTemperaturas.py
@@ -1,7 +1,8 @@
-'''Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. 
+'''FaÃ§a um Programa que peÃ§a a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. 
    C = 5 * ((F-32) / 9).
-   Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+   FaÃ§a um Programa que peÃ§a a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
 '''
+
 print("CONVERSOR CELSIUS <> FAHRENHEIT")
 print("###############################")
 
@@ -10,19 +11,19 @@ opcao = 1
 while opcao != 0:
     print("\nDigite:\n1. Celsius para Fahrenheit\n2. Fahrenheit para Celsius")
     print("Para sair, digite 0")
-    opcao = int(input("Opção: "))
+    opcao = int(input("OpÃ§Ã£o: "))
 
     if opcao == 1:
-        c = float(input("Digite a temperatura em °C: "))
+        c = float(input("Digite a temperatura em Â°C: "))
         f = (c * (9/5)) + 32
         f = round(f,2)
-        print("\nA temperatura é de:", f,"°F")
+        print("\nA temperatura Ã© de:", f,"Â°F")
     elif opcao == 2:
-        f = float(input("Digite a temperatura em °F: "))
+        f = float(input("Digite a temperatura em Â°F: "))
         c = 5 * ((f-32) / 9)
         c = round(c,2)
-        print("\nA temperatura é de:", c,"°C")
+        print("\nA temperatura Ã© de:", c,"Â°C")
     elif opcao == 0:
         print("\nSaindo...")
     else:
-        print("\nOpção incorreta. Tente novamente.")
+        print("\nOpÃ§Ã£o incorreta. Tente novamente.")
