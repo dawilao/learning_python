dc40ec7c51db31543aa91dd42a003565a62db252
Dawison Oliveira <96063474+dawilao@users.noreply.github.com>
2021-12-15 21:33:51 -0300

    Calculadora de valor de transporte
    Algoritmo que calcula o valor do transporte pela quantidade de dias úteis, além de calcular os dias úteis pelo total do transporte.

--- calculadora_valor_transporte.py
+++ calculadora_valor_transporte.py
@@ -1,26 +1,38 @@
+'''Algoritmo que calcula o valor do transporte pela quantidade de dias úteis, além de calcular os dias úteis pelo total do transporte.'''
+
 print('CALCULADORA DE VALOR DE TRANPORTE')
 print('#################################')
 
+x = True
 transp = 185.27
 uteis_mes = float(input('\nInforme a qntd. de dias úteis no mês (apenas números): '))
 
 transp_dia = transp/uteis_mes
 
-valid = input("Informe: dias ou valor (ou parar)? ")
-while valid != 'parar' and valid != 'dias' and valid != 'valor':
-    valid = input("Informe corretamente: dias ou valor (ou parar)? ")
+while x == True:
+  valid = input("Informe: dias ou valor (ou parar)? ")
+  while valid != 'parar' and valid != 'dias' and valid != 'valor':
+      valid = input("Informe corretamente: dias ou valor (ou parar)? ")
 
-while valid != 'parar':
-    if valid == 'dias':
-        uteis_trab = float(input('\nDias úteis trabalhados: '))
-        transp_total = uteis_trab * transp_dia
-        transp_total = round(transp_total,2)
-        print('Valor total: R$', transp_total)
-    elif valid == 'valor':
-        valor_total = float(input('\nValor aux. transp.: '))
-        uteis_trab = valor_total/transp_dia
-        uteis_trab = round(uteis_trab,2)
-        uteis_descont = uteis_mes - uteis_trab
-        print('Qntd. dias:', uteis_trab, '\nDias descontados:', uteis_descont)
-
-print('\nSaindo.')
+  while valid != 'parar':
+      if valid == 'dias':
+          uteis_trab = input('\nDias úteis trabalhados: ')
+          if uteis_trab == 'voltar':
+            break
+          else:
+            transp_total = float(uteis_trab) * transp_dia
+            transp_total = round(transp_total,2)
+            print('Valor total: R$', transp_total)
+      elif valid == 'valor':
+          valor_total = input('\nValor aux. transp.: ')
+          if valor_total == 'voltar':
+            break
+          else:
+            uteis_trab = float(valor_total)/transp_dia
+            uteis_trab = round(uteis_trab,2)
+            uteis_descont = uteis_mes - uteis_trab
+            print('Qntd. dias:', uteis_trab, '\nDias descontados:', uteis_descont)
+  
+  if valid == 'parar':
+    print('\nSaindo.')
+    x = False
