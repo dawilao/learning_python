e068e682b0bcd852ba7b2526acd793807450c569
Dawison Oliveira <96063474+dawilao@users.noreply.github.com>
2023-01-11 15:51:06 -0300

    math_game_1.1.py

--- math_game.py
+++ math_game.py
@@ -1,57 +1,78 @@
+'''
+JOGO DE MATEMÁTICA V1.1 (11/01/2023)
+'''
+
 from random import randint, randrange
 import time
 
-print ('JOGO DE MATEMÁTICA 1.0')
+print ('JOGO DE MATEMÁTICA')
 print('#######################')
 
 def saindo():
     print('\nObrigado por jogar!')
     time.sleep(3) #sleep por 3 seg
 
+def instrucoes():
+    print('\nINSTRUÇÕES')
+    print('#######################')
+    print('Acerte o máximo de questões que conseguir.')
+    print('Evite digitar letras nas questões.')
+
 sim = ['1', 'Sim', 'sim', 's']
-inst = ['2', 'Instrucoes', 'instrucoes', 'Instruções', 'instruções']
+inst = ['2', 'Instrucoes', 'instrucoes', 'Instruções', 'instruções', 'inst']
 sair = ['Sair', 'sair']
 
 num_question = 1
 pontuacao = 0
+iniciar = 0
 
-print('\nINSTRUÇÕES')
-print('#######################')
-print('Acerte o máximo de questões que conseguir.')
-print('Evite digitar letras nas questões.')
+instrucoes()
 
-iniciar = input('\nIniciar? ')
-print('Iniciando...')
+while iniciar == 0:
+    iniciar = input('\nIniciar? ')
+    if iniciar in sim or iniciar in inst:
+        while iniciar in sim:    
+            print('Iniciando...')
+            while iniciar in sim:
+                operacao = randint(1,2)
+                x = randint(0, 20)
+                y = randint(0, 20)
+                if operacao == 1:
+                    result = x + y
+                    resp = input('\n' + str(x) + ' + ' + str(y) + ' = ')
+                    if resp == str(result):
+                        pontuacao += 1
+                        print('Acertou! :)\nPontuação:', pontuacao)
+                    else:
+                        pontuacao -= 1
+                        print('Errou :(\nPontuação:', pontuacao)
+                    iniciar = input('Continuar? ')
+                else:
+                    result = x - y
+                    resp = input('\n' + str(x) + ' - ' + str(y) + ' = ')  
+                    if resp == str(result):
+                        pontuacao += 1
+                        print('Acertou! :)\nPontuação:', pontuacao)
+                    else:
+                        pontuacao -= 1
+                        print('Errou :(\nPontuação:', pontuacao)
+                    iniciar = input('Continuar? ')
+                    
+    while iniciar in inst:
+        print('elif instrucoes')
+        instrucoes()
+        iniciar = 0
+        continue
 
-while iniciar in sim:
-    operacao = randint(1,2)
-    x = randint(0, 20)
-    y = randint(0, 20)
-    if operacao == 1:
-        result = x + y
-        resp = input('\n' + str(x) + ' + ' + str(y) + ' = ')
-        if resp == str(result):
-            pontuacao += 1
-            print('Acertou! :)\nPontuação:', pontuacao)
-        else:
-            pontuacao -= 1
-            print('Errou :(\nPontuação:', pontuacao)
-        iniciar = input('Continuar? ')
-    else:
-        result = x - y
-        resp = input('\n' + str(x) + ' - ' + str(y) + ' = ')  
-        if resp == str(result):
-            pontuacao += 1
-            print('Acertou! :)\nPontuação:', pontuacao)
-        else:
-            pontuacao -= 1
-            print('Errou :(\nPontuação:', pontuacao)
-        iniciar = input('Continuar? ')
+    if iniciar in sair:
+        print('elif saindo')
+        saindo()
+        break;
+        '''Fechando o loop
+            elif resp in sair:
+                saindo()
+                break;  '''
 else:
+    print('a')
     saindo()
-
-'''Fechando o loop
-elif resp in sair:
-            saindo()
-            break;
-'''
+    
