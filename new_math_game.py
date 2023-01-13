cacea2f1ba51d8d5c33b01dc547b0c0deacf7f0b
Dawison Oliveira <daw_afk@tutamail.com>
2023-01-12 22:24:02 -0300

    Continuação da revisão

--- new_math_game.py
+++ new_math_game.py
@@ -4,6 +4,7 @@ JOGO DE MATEMÁTICA V1.9 (12/01/2023)
 
 from random import randint, randrange
 import time
+import os
 
 def menu_principal():
     print ('JOGO DE MATEMÁTICA')
@@ -21,23 +22,194 @@ def instrucoes():
     print('#######################')
     print('Acerte o máximo de questões que conseguir.')
 
+def nao_iniciar():
+    iniciar = True
+    print('Voltando...')
+    time.sleep(1.3)
+    os.system('cls') # Clearing the Screen
+    return
+
+def perdeu():
+    iniciar = True
+    print('Game over. Você não tem mais vidas.\nPontuação final:', pontuacao, '\n')
+    print('Voltando ao menu principal...')
+    time.sleep(4)
+    os.system('cls') # Clearing the Screen
+
 sim = ['1', 'Sim', 'sim', 's', 'S', 'iniciar', 'INICIAR', 'Iniciar']
 inst = ['2', 'Instrucoes', 'instrucoes', 'Instruções', 'instruções', 'inst']
-sair = ['Sair', 'sair', 'SAIR', "não", 'n', 'N', 'nao', 'NÃO']
+sair = ['Sair', 'sair', 'SAIR']
+nao = ["não", 'n', 'N', 'nao', 'NÃO']
 selec_vidas = ['vidas', 'v']
 
 num_question = 1
 pontuacao = 0
 iniciar = True
 rodada = 1
-vidas = 3
+vidas = vidas_selecionadas = 1
 
-menu_principal()
 while iniciar == True:
+    menu_principal()
     iniciar = input('> ')
-    if iniciar in sim:        
-        print("rodar jogo")
+    while iniciar not in sim and inst and selec_vidas: #AND, não OR
+        if iniciar in sair:
+            break
+        while iniciar in inst:
+            instrucoes()
+            iniciar = input('\nIniciar jogo? ')
+        while iniciar in selec_vidas:
+            while True:
+                try:
+                    vidas_selecionadas = int(input('Selecione a quantidade de vidas (1 ou 3): '))
+                    break
+                except ValueError:
+                    print('Apenas números.')
+            iniciar = input('\nIniciar jogo? ') # FORA do try 
+        if iniciar in nao: # if para voltar ao menu/primeiro while
+            iniciar = True
+            print('Voltando...')
+            time.sleep(1.3)
+            os.system('cls') # Clearing the Screen
+            break
+        elif iniciar in sair:
+            break
+        elif iniciar in sim:
+            continue
+        else:
+            iniciar = input('Tente novamente.\n> ')
     else:
-        break
+        while iniciar in sim:        
+            print('Iniciando...')
+            vidas = vidas_selecionadas
+            while iniciar in sim and vidas > 0:
+                print('\nRodada', rodada, ' Vidas:', vidas, ' Pontuação:', pontuacao)
+                operacao = randint(1, 4)
+                operacao = 3
+                x = randint(0, 50)
+                y = randint(0, 50)
+                if operacao == 1:
+                    result = x + y
+                    while True:
+                        try:
+                            resp = int(input('' + str(x) + ' + ' + str(y) + ' = '))
+                            break
+                        except ValueError:
+                            print('Apenas números.')
+                    if str(resp) == str(result):
+                        pontuacao += 1
+                        print('Acertou! :)')
+                        rodada += 1
+                    else:
+                        pontuacao -= 1
+                        print('Errou :( Você perdeu uma vida.\nResposta correta:', result)
+                        rodada += 1
+                        vidas -= 1
+                    if vidas == 0:
+                        iniciar = True
+                        print('Game over. Você não tem mais vidas.\nPontuação final:', pontuacao, '\n')
+                        print('Voltando ao menu principal...')
+                        time.sleep(4)
+                        os.system('cls') # Clearing the Screen
+                        break
+                    else:
+                        iniciar = input('Continuar? ')
+                elif operacao == 2:
+                    if y > x:
+                        result = y - x
+                        while True:
+                            try:
+                                resp = int(input('' + str(y) + ' - ' + str(x) + ' = '))
+                                break
+                            except ValueError:
+                                print('Apenas números.')
+                    else:
+                        result = x - y
+                        while True:
+                            try:
+                                resp = int(input('' + str(x) + ' - ' + str(y) + ' = '))
+                                break
+                            except ValueError:
+                                print('Apenas números.')
+                    if str(resp) == str(result):
+                        pontuacao += 1
+                        print('Acertou! :)')
+                        rodada += 1
+                    else:
+                        pontuacao -= 1
+                        print('Errou :( Você perdeu uma vida.\nResposta correta:', result)
+                        rodada += 1
+                        vidas -= 1
+                    if vidas == 0:
+                        iniciar = True
+                        print('Game over. Você não tem mais vidas.\nPontuação final:', pontuacao, '\n')
+                        print('Voltando ao menu principal...')
+                        time.sleep(4)
+                        os.system('cls') # Clearing the Screen
+                        break
+                    else:
+                        iniciar = input('Continuar? ')
+                elif operacao == 3:
+                    x = randint(0, 10)
+                    y = randint(0, 10)
+                    result = x * y
+                    while True:
+                        try:
+                            resp = int(input('' + str(x) + ' x ' + str(y) + ' = '))
+                            break
+                        except ValueError:
+                            print('Apenas números.')
+                    if str(resp) == str(result):
+                        pontuacao += 1
+                        print('Acertou! :)')
+                        rodada += 1
+                    else:
+                        pontuacao -= 1
+                        print('Errou :( Você perdeu uma vida.\nResposta correta:', result)
+                        rodada += 1
+                        vidas -= 1
+                    if vidas == 0:
+                        iniciar = True
+                        print('Game over. Você não tem mais vidas.\nPontuação final:', pontuacao, '\n')
+                        print('Voltando ao menu principal...')
+                        time.sleep(4)
+                        os.system('cls') # Clearing the Screen
+                        break
+                    else:
+                        iniciar = input('Continuar? ')
+                else:
+                    x = randint(0, 100)
+                    y = randint(1, 100)
+                    resto = x % y
+                    while resto != 0:
+                        x = randint(0, 100)
+                        y = randint(1, 100)
+                        resto = x % y
+                    result = x // y
+                    while True:
+                        try:
+                            resp = int(input('' + str(x) + ' / ' + str(y) + ' = '))
+                            break
+                        except ValueError:
+                            print('Apenas números.')
+                    if str(resp) == str(result):
+                        pontuacao += 1
+                        print('Acertou! :)')
+                    else:
+                        pontuacao -= 1
+                        print('Errou :(\nResposta correta:', result)
+                        rodada += 1
+                        vidas -= 1
+                    if vidas == 0:
+                        iniciar = True
+                        print('Game over. Você não tem mais vidas.\nPontuação final:', pontuacao, '\n')
+                        print('Voltando ao menu principal...')
+                        time.sleep(4)
+                        os.system('cls') # Clearing the Screen
+                        break
+                    else:
+                        iniciar = input('Continuar? ')
+                        break
+            if vidas == 0:
+                break
 else:
     saindo()
