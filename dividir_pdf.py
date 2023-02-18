996d394d1920684ac37c4d449e7cd41a8d2327e3
Dawison Oliveira <daw_afk@tutamail.com>
2023-02-17 22:52:43 -0300

    Alterações Changes to be committed:     modified:   dividir_pdf.py
    Changes not staged for commit:
        modified:   rastreio_correios_v2.0.py
    	deleted:    renomear_bloquear_pdf.py
    	deleted:    sys.py
    
    Renamed:
    renomear_bloquear_pdf.py > Editor_IRs.py
    sys.py > args_padroes.py

--- dividir_pdf.py
+++ dividir_pdf.py
@@ -4,7 +4,7 @@ import os
 import PyPDF4
 
 # Define uma função para dividir um PDF em arquivos contendo 1 página cada.
-def dividir_pdf_1(diretorio):
+def dividir_pdf_1(diretorio, diretorio_saida):
     # Obtém o nome do arquivo original, sem extensão.
     nome_arquivo = os.path.splitext(os.path.basename(diretorio))[0]
     
@@ -17,14 +17,15 @@ def dividir_pdf_1(diretorio):
 
         # Define o nome do arquivo de saída e escreve o PDF atual para ele.
         nome_arquivo_saida = '{}_{}.pdf'.format(nome_arquivo, pagina)
-        with open(nome_arquivo_saida, 'wb') as saida:
+        nome_completo_saida = os.path.join(diretorio_saida, nome_arquivo_saida)
+        with open(nome_completo_saida, 'wb') as saida:
             escreve_pdf.write(saida)
         
         # Imprime uma mensagem indicando que o arquivo de saída foi criado.
         print('Criado: {}'.format(nome_arquivo_saida))
 
 # Define uma função para dividir um PDF em arquivos contendo 2 páginas cada.
-def dividir_pdf_2(diretorio):
+def dividir_pdf_2(diretorio, diretorio_saida):
     # Obtém o nome do arquivo original, sem extensão.
     nome_arquivo = os.path.splitext(os.path.basename(diretorio))[0]
 
@@ -37,9 +38,10 @@ def dividir_pdf_2(diretorio):
             escreve_pdf.addPage(pdf.getPage(pagina))
             escreve_pdf.addPage(pdf.getPage(pagina + 1))
             nome_arquivo_saida = '{}_{}.pdf'.format(nome_arquivo, pagina // 2 + 1)
-        
+            nome_completo_saida = os.path.join(diretorio_saida, nome_arquivo_saida)
+
             # Define o nome do arquivo de saída e escreve o PDF atual para ele.
-            with open(nome_arquivo_saida, 'wb') as saida:
+            with open(nome_completo_saida, 'wb') as saida:
                 escreve_pdf.write(saida)
             
             # Imprime uma mensagem indicando que o arquivo de saída foi criado.
@@ -50,6 +52,9 @@ pasta = input('Digite o caminho para encontrar o PDF a ser dividido: ')
 arquivo = input('Digite o nome do arquivo: ')
 diretorio = os.path.join(pasta, arquivo + '.pdf') 
 
+# Obtém o caminho para o diretório onde os arquivos de saída devem ser salvos.
+diretorio_saida = input('Digite o caminho para o diretório onde os arquivos de saída devem ser salvos: ')
+
 # Pergunta ao usuário se ele deseja dividir o PDF em arquivos com 1 ou 2 páginas cada.
 dividir = int(input('Dividir a cada 1 ou 2 páginas? > '))
 while dividir != 1 and dividir != 2:
@@ -57,8 +62,8 @@ while dividir != 1 and dividir != 2:
 
 # Chama a função apropriada com base na escolha do usuário.
 if dividir == 1:
-    dividir_pdf_1(diretorio)
+    dividir_pdf_1(diretorio, diretorio_saida)
 else:
-    dividir_pdf_2(diretorio)
+    dividir_pdf_2(diretorio, diretorio_saida)
 
 input('Tecle Enter para sair.')
